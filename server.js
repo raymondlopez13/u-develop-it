cbe49cc2662cb5e4c4a65742d57d8ca6de91352d
Raymond Lopez <raymondlopez13@yahoo.com>
2021-03-08 18:30:11 -0600

    Added get route for home page, that returns nothing, set up express

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const PORT = process.env.PORT || 3001;
+const app = express();
+//middleware
+app.use(express.urlencoded({ extended: false}));
+app.use(express.json());
+
+
+// routes
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Hello World'
+    });
+});
+// Catch all
+app.use((req,res) => {
+    res.status(404).end();
+});
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
