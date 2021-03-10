94f390b6eec247c7d9c1196e29f08cb3a5827a92
Raymond Lopez <raymondlopez13@yahoo.com>
2021-03-10 11:28:25 -0600

    Populated parties table and linked it to candidates table

--- server.js
+++ server.js
@@ -23,7 +23,11 @@ app.get('/', (req, res) => {
 });
 // api endpoint for all candidates
 app.get('/api/candidates', (req, res) => {
-    const sql = `SELECT * FROM candidates`;
+    const sql = `SELECT candidates.*, parties.name
+    AS party_name
+    FROM candidates
+    LEFT JOIN parties
+    ON candidates.party_id = parties.id`;
     const params = [];
     db.all(sql, params, (err,rows) => {
         if (err) {
@@ -39,7 +43,12 @@ app.get('/api/candidates', (req, res) => {
 });
 // api endpoint for single candidate
 app.get('/api/candidate/:id', (req, res) => {
-    const sql = `SELECT * FROM candidates WHERE id = ?`;
+    const sql = `SELECT candidates.*, parties.name
+    AS party_name
+    FROM candidates
+    LEFT JOIN parties
+    ON candidates.party_id = parties.id
+    WHERE candidates.id = ?`;
     const params = [req.params.id];
     db.get(sql, params, (err, row) => {
         if (err) {
